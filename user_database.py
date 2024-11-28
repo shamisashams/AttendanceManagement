5882424dec3ad24317d69477923f3ef7b3d42534
Shammy <76426876+shamisashams@users.noreply.github.com>
2024-11-28 11:44:05 +0100

    load_users() added to userdatabase

--- user_database.py
+++ user_database.py
@@ -2,12 +2,16 @@ import face_recognition
 import json
 
 class UserDatabase:
-    def __init__(self):
-        # self.add_user
-        pass
+    def __init__(self, db_path = 'users.json'):
+        self.db_path = db_path
+        self.users = self.load_users()
 
-    def load_users():
-        pass
+    def load_users(self):
+        try:
+            with open(self.db_path, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return {}
 
     def add_user(self, name, user_id, image_path):
         image = face_recognition.load_image_file(image_path)
