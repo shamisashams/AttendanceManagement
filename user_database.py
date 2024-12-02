f792075ea29610cae2c9acac34ed475264b48b31
Shammy <76426876+shamisashams@users.noreply.github.com>
2024-12-02 07:43:43 +0100

    user_database updated

--- user_database.py
+++ user_database.py
@@ -16,7 +16,9 @@ class UserDatabase:
     def add_user(self, name, user_id, image_path):
         image = face_recognition.load_image_file(image_path)
         encoding = face_recognition.face_encodings(image)[0]
-        print(image)
+        self.users[user_id] = {'name': name, 'encoding': encoding.tolist()}
+        self.save_users()
 
-    def save_users():
-        pass
+    def save_users(self):
+        with open(self.db_path, 'w') as file:
+            json.dump(self.users, file)
