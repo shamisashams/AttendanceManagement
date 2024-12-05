404c010e86b45d15dafe7b5f5a511ea9964dfeac
Shammy <76426876+shamisashams@users.noreply.github.com>
2024-12-05 08:28:43 +0100

    atterndance manager class created

--- attendance_manager.py
+++ attendance_manager.py
@@ -1,1 +1,9 @@
+class AttendanceManager:
+    def __init__(self, user_db):
+        self.user_db = user_db
 
+    def mark_attendance(self):
+        attendance = []
+
+    def generate_report(self):
+        pass
