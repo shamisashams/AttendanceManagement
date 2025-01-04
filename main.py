7c15ba6d9928a296400548a8b00c1f476d5bbd9c
Shammy <76426876+shamisashams@users.noreply.github.com>
2025-01-04 09:56:43 +0100

    main.py updated

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from face_detector import FaceDetector
 from user_database import UserDatabase
 from attendance_manager import AttendanceManager
+from face_recognition_attendance import FaceRecognitionAttendanceSystem
+from manual_attendance import ManualAttendanceSystem
 
-database = UserDatabase()
+db = UserDatabase()
 detector = FaceDetector()
-manager = AttendanceManager(database)
+manager = AttendanceManager(db)
 
-database.add_user("Bill Gates", "001", "img/known/bill_gates.jpg")
-database.add_user("Steve Jobs", "002", "img/known/steve_jobs.jpg")
-database.add_user("Elon Musk", "003", "img/known/elon_musk.jpg")
-attendance = manager.mark_attendance("img/groups/bill-steve-elon.jpg")
-manager.generate_report(attendance)
+db.add_user("Bill Gates", "001", "images/known/bill_gates.jpg")
+db.add_user("Steve Jobs", "002", "images/known/steve_jobs.jpg")
+db.add_user("Elon Musk", "003", "images/known/elon_musk.jpg")
 
-print(attendance)
+# Facial Recognition Attendance System
+face_recognition_system = FaceRecognitionAttendanceSystem(db)
+face_recognition_system.mark_attendance_from_image("images/group_photo.jpg")
+
+# Manual Attendance System
+manual_system = ManualAttendanceSystem()
+manual_system.mark_attendance_manually("002", "Jane Smith")
+
+
+# View Attendance Logs
+print("\nFacial Recognition System Logs:")
+face_recognition_system.view_attendance_log()
+
+print("\nManual Attendance System Logs:")
+manual_system.view_attendance_log()
